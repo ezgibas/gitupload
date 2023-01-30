0568f047a13996e8e121a762bc441cfd5e3663ea
Ezgi Bas <52749796+ezgibas@users.noreply.github.com>
2023-01-30 15:48:49 -0500

    test

--- cool.js
+++ cool.js
@@ -1,6 +1,7 @@
 var XMLHttpRequest = require('xhr2');
 var commits = "https://api.github.com/repos/ezgibas/gitupload/commits";
 test(commits);
+
 function test(input, fileInput) {
     const http = new XMLHttpRequest();
     // get the commits data
